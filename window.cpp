d8364959bffca950d2be7b7953598f0b4bf41fde
paulojamorim <paulojamorim@gmail.com>
2014-01-31 21:50:05 -0200

    Open window

--- window.cpp
+++ window.cpp
@@ -14,4 +14,35 @@ int main()
         fprintf(stderr, "Problem to initialize GLFW\n");
         return -1;
     }
+
+    glfwOpenWindowHint(GLFW_FSAA_SAMPLES, 4);
+    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
+    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);
+    glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    if(!glfwOpenWindow(800, 600, 0, 0, 0, 0, 32, 0, GLFW_WINDOW))
+    {
+        fprintf(stderr, "Failed to open GLFW Window\n");
+        glfwTerminate();
+    return -1;
+    }
+
+    glewExperimental=true;
+    
+    if(glewInit() != GLEW_OK)
+    {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        return -1;
+    }
+
+    glfwSetWindowTitle("First Tutorial");
+
+    glfwEnable( GLFW_STICKY_KEYS );
+
+    do
+    {
+    
+        glfwSwapBuffers();
+    }
+    while(glfwGetKey(GLFW_KEY_ESC) != GLFW_PRESS && glfwGetWindowParam(GLFW_OPENED));
 }
